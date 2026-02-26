3e6e222cfd761bb3031fab42dd5e365eb237f148
Anish Roy <aroy262692@gmail.com>
2026-02-26 10:08:05 +0530

    add: implement Reveal, StaggerContainer, and StaggerItem components

---
+++ components/shared/index.ts
@@ -0,0 +1,4 @@
+export { default as Reveal } from "./Reveal";
+export { default as StaggerContainer } from "./StaggerContainer";
+export { default as StaggerItem } from "./StaggerItem";
+export { default as Navbar } from "./Navbar";
